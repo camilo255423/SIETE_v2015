697936dcd5eed5598f1c1f6e979e96b975a01a42
Camilo Rodriguez <rodriguez.camilo@gmail.com>
2016-04-09 10:38:17 -0500

    creación del modelo área

---
+++ app/models/Area.java
@@ -0,0 +1,105 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import play.db.DB;
+
+public class Area {
+	String codArea;
+	String nombre;
+	public Area(String codArea, String nombre) {
+		super();
+		this.codArea = codArea;
+		this.nombre = nombre;
+	}
+	public static List<Area> findAll()
+	{
+     	Connection con = DB.getConnection();
+		List<Area> areas = new ArrayList<Area>();
+		PreparedStatement p;
+		try {
+			p = con.prepareStatement(consultaAreas);
+			
+		
+			ResultSet rs=p.executeQuery();
+			while (rs.next()) {
+				areas.add(new Area(rs.getString("CENTRO_COSTO"),
+						rs.getString("NOMBRE")));
+			}
+			  con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Excepcion : "+e.getMessage());
+			System.out.println(e.getLocalizedMessage());
+		}
+	
+		
+		
+		return areas;
+	}
+	public static Area findById(String id)
+	{
+     	Connection con = DB.getConnection();
+		Area area=null;
+		PreparedStatement p;
+		try {
+			p = con.prepareStatement(consultaIdArea);
+			
+			p.setString(1, id);
+		
+			ResultSet rs=p.executeQuery();
+			while (rs.next()) {
+				area = new Area(rs.getString("CENTRO_COSTO"),
+						rs.getString("NOMBRE"));
+			}
+			con.close();	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Excepcion : "+e.getMessage());
+			System.out.println(e.getLocalizedMessage());
+		}
+	
+		
+		
+		return area;
+	}
+	public static List<Area> findAllByCoordinador(String cedula)
+	{
+     	Connection con = DB.getConnection();
+		List<Area> areas = new ArrayList<Area>();
+		PreparedStatement p;
+		try {
+			p = con.prepareStatement(consultaAreasCoordinador);
+			p.setString(1, cedula);
+			ResultSet rs=p.executeQuery();
+			while (rs.next()) {
+				areas.add(new Area(rs.getString("CENTRO_COSTO"),
+						rs.getString("NOMBRE")));
+			}
+			  con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Excepcion : "+e.getMessage());
+			System.out.println(e.getLocalizedMessage());
+		}
+	
+		
+		
+		return areas;
+	}
+
+	private static final String consultaAreas ="select * from AREA_CENTRO_COSTO";
+	private static final String consultaIdArea ="select * from AREA_CENTRO_COSTO where CENTRO_COSTO=?";
+	private static final String consultaAreasCoordinador="select * from AREA_CENTRO_COSTO, PERMISO "+ 
+	"where PERMISO.CENTRO_COSTO_PROGRAMA=AREA_CENTRO_COSTO.CENTRO_COSTO and ID_ROL=4 and CEDULA=?";
+
+
+}
